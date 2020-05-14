7e076176417156a88ac45587b5135ba3fb0f5cf5
DoanHieu_IQ <54270198+doanhieucodegym@users.noreply.github.com>
2020-05-14 15:13:17 +0700

    return max element

--- src/com/hieudv/javaco_coban/elmentMaxinList/ElementMax.java
+++ src/com/hieudv/javaco_coban/elmentMaxinList/ElementMax.java
@@ -1,5 +1,21 @@
 package com.hieudv.javaco_coban.elmentMaxinList;
 
 public class ElementMax {
+    public static void main(String[] args) {
+        // su dụng ham
+        int[] arr = {1,3,4,5,10};
+      int max =max(arr ,arr.length);
+        System.out.println("so lon nhat" + max);
 
+
+    }
+    public static int max(int[] a, int n){
+        int temp =a[0];
+        for(int i =0 ; i <a.length; i++){
+            if(a[i] > temp){
+                temp = a[i] ;
+            }
+        }
+        return temp;
+    }
 }
